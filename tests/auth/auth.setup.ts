317e84b3d93812c12972d31e8667f4e984ac9961
AncaR <anca.rada24@gmail.com>
2026-05-07 09:07:56 +0200

    add wait for email because angular might be slow

--- tests/auth/auth.setup.ts
+++ tests/auth/auth.setup.ts
@@ -5,6 +5,7 @@ const authFile = path.join(__dirname, '../../.auth/user.json');
 
 setup('auth', async ({page})=> {
     await page.goto('/auth/login');
+    await page.waitForSelector('[data-test="email"]')
     await page.fill('[data-test="email"]', process.env.USER_EMAIL as string);
     await page.fill('[data-test="password"]', process.env.USER_PASSWORD as string)
     await page.click('[data-test="login-submit"]')
